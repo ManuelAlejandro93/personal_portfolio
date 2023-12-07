00a48eac86f9e6420350bfebce4bc3fd13b9f88b
Manuel Aguirre <clasefisica2019@gmail.com>
2023-12-07 18:12:40 -0500

    menu hamburguesa terminado

--- src/app/components/molecules/menuHamburguesa/MenuHamburguesa.jsx
+++ src/app/components/molecules/menuHamburguesa/MenuHamburguesa.jsx
@@ -1,16 +1,32 @@
+'use client'
 import Link from "next/link";
 import styles from './MenuHamburguesa.module.css';
+import { useState,useEffect } from "react";
+import {asignarClaseParaElMenuDesplegable} from './funciones/funcionesMenuHamburguesa';
 
 export default function MenuHamburguesa() {
- 
+  const [estadoGatillo,setEstadoGatillo] = useState(false);
+
+  const [estadoDesplegable,setEstadoDesplegable] = useState(`${styles.menuDesplegable} ${styles.menuDesplegableInvisible}`);
+
+  useEffect(() => {
+    if(estadoGatillo){
+      asignarClaseParaElMenuDesplegable(estadoGatillo,estadoDesplegable,setEstadoDesplegable,styles)
+    } else {
+      asignarClaseParaElMenuDesplegable(estadoGatillo,estadoDesplegable,setEstadoDesplegable,styles)
+    }
+  }, [estadoGatillo])
+  
   return (
     <>
       <div className={styles.macroMenuHamburguesa}>
-        <input
-          type="checkbox"
+        <button
           className={styles.gatilloMenu}
           id="gatilloMenu"
-        />
+          onClick={()=>{setEstadoGatillo(!estadoGatillo)}}
+        >
+        </button>
+
         <label className={styles.labelDeGatillo} htmlFor="gatilloMenu">
           <div className={styles.hamburguesa}>
             <div className={styles.linea}></div>
@@ -18,28 +34,28 @@ export default function MenuHamburguesa() {
             <div className={styles.linea}></div>
           </div>
         </label>
-        <div className={styles.menuDesplegable}>
-          <Link className={styles.links} href={"/"}>
+        <div className={estadoDesplegable}>
+          <Link className={styles.links} href={"/"} onClick={()=>{setEstadoGatillo(!estadoGatillo)}}>
             Inicio
           </Link>
 
-          <Link className={styles.links} href={"/pages/AcercaDeMi"}>
+          <Link className={styles.links} href={"/pages/AcercaDeMi"} onClick={()=>{setEstadoGatillo(!estadoGatillo)}}>
             Acerca de mi
           </Link>
 
-          <Link className={styles.links} href={"/pages/Proyectos"}>
+          <Link className={styles.links} href={"/pages/Proyectos"} onClick={()=>{setEstadoGatillo(!estadoGatillo)}}>
             Proyectos
           </Link>
 
-          <Link className={styles.links} href={"/pages/Habilidades"}>
+          <Link className={styles.links} href={"/pages/Habilidades"} onClick={()=>{setEstadoGatillo(!estadoGatillo)}}>
             Habilidades
           </Link>
 
-          <Link className={styles.links} href={"/pages/Estudios"}>
+          <Link className={styles.links} href={"/pages/Estudios"} onClick={()=>{setEstadoGatillo(!estadoGatillo);console.log("Ahora o soy ", estadoGatillo)}}>
             Estudio
           </Link>
 
-          <Link className={styles.links} href={"/pages/Contacto"}>
+          <Link className={styles.links} href={"/pages/Contacto"} onClick={()=>{setEstadoGatillo(!estadoGatillo);console.log("Ahora o soy ", estadoGatillo)}}>
             Contacto
           </Link>
         </div>
--- src/app/components/molecules/menuHamburguesa/funciones/funcionesMenuHamburguesa.js
+++ src/app/components/molecules/menuHamburguesa/funciones/funcionesMenuHamburguesa.js
@@ -1,3 +1,9 @@
-// export function esconderMenuDesplegable(){
-//     console.log('Estoy dentro de la funcion menú desplegable.');
-// }
+export function asignarClaseParaElMenuDesplegable(estadoGatillo,estadoDesplegable, setEstadoDesplegable, styles) {
+  if (estadoGatillo) {
+    setEstadoDesplegable(`${styles.menuDesplegableInvisible}`);
+    console.log(estadoDesplegable);
+  } else {
+    setEstadoDesplegable(`${styles.menuDesplegableVisible}`);
+    console.log(estadoDesplegable);
+  }
+}
