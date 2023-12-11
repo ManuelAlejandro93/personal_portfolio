0a067fc59bb9f7044f8bcb76bd95e9fad918e9e0
Manuel Aguirre <clasefisica2019@gmail.com>
2023-12-11 08:32:16 -0500

    cambios en la vista de inicio

--- src/app/components/molecules/contenidoInicio/ContenidoInicio.jsx
+++ src/app/components/molecules/contenidoInicio/ContenidoInicio.jsx
@@ -1,16 +1,14 @@
 import Image from "next/image";
-import stylesContenidoInicio from "./ContenidoInicio.module.css";
+import styles from "./ContenidoInicio.module.css";
 
 export default function ContenidoInicio() {
   return (
     <> 
-       <main className={stylesContenidoInicio.main}>
-        <div className={stylesContenidoInicio.contenido}>
-          <div className={stylesContenidoInicio.textosHeader}>
-            <h1 className={stylesContenidoInicio.h1Header}>Manuel Pinzón Aguirre</h1>
-            <h3 className={stylesContenidoInicio.advertencia}>Algunas partes de esta página se encuentran en reparación</h3>
-            <h3 className={stylesContenidoInicio.advertencia}>más info: +573143415131</h3>
-            <p className={stylesContenidoInicio.pHeader}>Front-End Developer: Next.js, React.js, Javascript.</p>
+       <main className={styles.main}>
+        <div className={styles.contenido}>
+          <div className={styles.textosHeader}>
+            <h1 className={styles.h1Header}>Manuel Pinzón Aguirre</h1>
+            <p className={styles.pHeader}>Front-End Developer: Next.js, React.js, Javascript.</p>
           </div>
 
           <Image
@@ -18,7 +16,7 @@ export default function ContenidoInicio() {
             width={270}
             height={270}
             alt="Foto del Dev Manuel"
-            className={stylesContenidoInicio.ImagenManuel}
+            className={styles.ImagenManuel}
             priority={true}
           ></Image>
         </div>
