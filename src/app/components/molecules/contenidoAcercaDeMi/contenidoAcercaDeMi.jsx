6d660f76e9f5f89cc06fab50e37d81c7fa69eef6
Manuel Aguirre <clasefisica2019@gmail.com>
2023-12-11 12:49:24 -0500

    algunos adelantos de los cambios

--- src/app/components/molecules/contenidoAcercaDeMi/contenidoAcercaDeMi.jsx
+++ src/app/components/molecules/contenidoAcercaDeMi/contenidoAcercaDeMi.jsx
@@ -6,25 +6,40 @@ export default function ContenidoAcercaDeMi() {
     <>
       <main className={styles.main}>
         <div className={styles.contenedor}>
-          <Image src="/fotoAcercaDeMi/foto.jpeg"
+          <Image
+            src="/fotoAcercaDeMi/foto.jpeg"
             width={600}
             height={400}
             alt="Foto del Dev Manuel"
             priority={true}
-            className= {styles.foto}></Image>
-          
-          <div className= {styles.divDerecha}>
-            <p className= {`${styles.parrafo} ${styles.parrafo1}`}>
-            Me llamo Manuel Pinzón. Soy un desarrollador FrontEnd y un deportista amateur muy competitivo.
+            className={styles.foto}
+          ></Image>
+          <div className={styles.divDerecha}>
+            <p className={`${styles.parrafos} ${styles.parrafo1}`}>
+              Me llamo Manuel Pinzón. Soy un desarrollador FrontEnd y un
+              deportista amateur muy competitivo.
             </p>
-            <p className= {`${styles.parrafo} ${styles.parrafo2}`}>Además me he especialiado en desarrollo front-end y encuentro mucha motivacion el desarrollo web en general. </p>
-
-            <p className={`${styles.parrafo} ${styles.parrafo3}`} >Mis motivaciones en la vida se mantienen en un buen vivir diario, y en el aprecio de las cosas pequeñas. Disfruto mucho los nuevos retos que me recuerden que la vida es bella y retadora en sus facetas más simples y más complejas.</p>
-
-            <p className= {`${styles.parrafo} ${styles.parrafo4}`}>Conocí la programación cuando tuve que estudiar C++ para los proyectos de la universidad, y sinceramente debo admitir que hicimos match automático. Poder hablar el lenguaje de las maquinas es simplemente fascinante.
+            <p className={`${styles.parrafos} ${styles.parrafo2}`}>
+              Soy un operador del Framework Next.js y la libería React.js, además he tenido cercania a Material UI Dart, Flutter, SQL, PL/SQL y Python de manera profesional.
             </p>
 
-            <p className= {`${styles.parrafo} ${styles.parrafo5}`}>En esta etapa de mi vida me apasiona el desarrollo front-end, en la tecnología React y el framework Next.
+            <p className={`${styles.parrafos} ${styles.parrafo3}`}>
+              Mis motivaciones en la vida se mantienen en un buen vivir diario,
+              y en el aprecio de las cosas pequeñas. Disfruto mucho los nuevos
+              retos que me recuerden que la vida es bella y retadora en sus
+              facetas más simples y más complejas.
+            </p>
+
+            <p className={`${styles.parrafos} ${styles.parrafo4}`}>
+              Conocí la programación cuando tuve que estudiar C++ para los
+              proyectos de la universidad, y sinceramente debo admitir que
+              hicimos match automático. Poder hablar el lenguaje de las maquinas
+              es simplemente fascinante.
+            </p>
+
+            <p className={`${styles.parrafos} ${styles.parrafo5}`}>
+              En esta etapa de mi vida me apasiona el desarrollo front-end, en
+              la tecnología React y el framework Next.
             </p>
           </div>
         </div>
